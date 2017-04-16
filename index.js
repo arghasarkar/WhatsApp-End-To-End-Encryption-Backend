a7dbd2f4e650cf04626d9d4b617c18d34287b05d
Argha Sarkar <argha.sarkar1994@gmail.com>
2017-04-16 01:18:01 +0100

    WIP

--- index.js
+++ index.js
@@ -1,38 +1,52 @@
-var ORM = require('postgresql-orm');
+"use strict";
+const POSTGRES_CONNECTION_STRING = 'postgresql://ejjdtmnmgsuwvk:7550269a2ee3a15ff562f7a4fef2f2681d4b160419518a61f5628c492842ff6d@ec2-54-228-235-185.eu-west-1.compute.amazonaws.com:5432/d7ijml58sqdn92?ssl=true';
 
-ORM.setup('postgresql://ejjdtmnmgsuwvk:7550269a2ee3a15ff562f7a4fef2f2681d4b160419518a61f5628c492842ff6d@ec2-54-228-235-185.eu-west-1.compute.amazonaws.com:5432/d7ijml58sqdn92?ssl=true');
+let express = require("express");
+let app = express();
+const PORT    =   process.env.PORT || 8080;
+let APIRouter = express.Router();
 
-var userEntityDefinition = {
+let ORM = require('postgresql-orm');
+
+ORM.setup(POSTGRES_CONNECTION_STRING);
+
+
+let userEntityDefinition = {
     name: 'users', // will match table with name 'users'
     attributes: {
+        /*id: {
+            type: '	autoincrementing integer',
+            unique: true
+        },*/
         email: {
             type: 'character varying',
         },
         name: {
             type: 'character varying'
         },
-        publickey: {
+        public_key: {
             type: 'character varying'
         }
     }
 };
 
-var User = ORM.define(userEntityDefinition)
-
-/*User.dropTable(function(err) {
+let User = ORM.define(userEntityDefinition);
+/*
+/!*User.dropTable(function(err) {
     // existing table dropped
 });
 
 User.createTable(function(err) {
     // table created
-});*/
+});*!/
 
-User.save({name: 'John', email:"John@john.com", publickey:"johnpk"}, function(err, savedEntity) {
+User.save({id: null ,name: 'Jozzzzhna', email:"Johan@johna.com", public_key:"johnapk"}, function(err, savedEntity) {
     // do something
-    savedEntity.id;
+    console.log(err);
+    console.log(savedEntity);
 });
 
-/*
+/!*
 User.create({firstName: 'John'}, function(err, createdEntity) {
     // do smthg
 })
@@ -40,9 +54,50 @@ User.create({firstName: 'John'}, function(err, createdEntity) {
 User.update({id: 123, lastName: 'Doe'}, function(err, updatedEntity) {
     // do smthg
 });
-*/
-
-User.load({id: 2}, function(err, loadedEntity) {
+*!/
+ */
+User.load({"id": 3}, function(err, loadedEntity) {
+    console.log(err);
     // do smthg
     console.log(loadedEntity);
+});
+
+
+function loadUserByID(userID) {
+
+    return new Promise(function (resolve, reject) {
+        User.load({id: 6},   function (err, loadedEntity) {
+            if (err === undefined) {
+                console.log(loadedEntity);
+                resolve(loadedEntity);
+            } else {
+                reject(err);
+            }
+        })
+    });
+}
+
+/**
+ * Routes for the API.
+ */
+APIRouter.get("/", function (req, res, err) {
+   res.send("API Hit");
+});
+
+APIRouter.get("/user_id/:user_id", function (req, res, err) {
+   let userID = req.params.user_id;
+   console.log(userID);
+   loadUserByID(userID).then(function (userData) {
+      res.send(userData);
+   }).catch(function (err) {
+      res.status(404);
+      res.send({error: err});
+   });
+});
+
+app.use("/api", APIRouter);
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}.`);
 });
--- schema.sql
+++ schema.sql
@@ -7,9 +7,10 @@ CREATE TABLE users (
 	public_key TEXT NOT NULL
 );
 
-INSERT INTO public.users (id, name, email, public_key) VALUES (1, 'a', 'a@a.com', 'akey');
-INSERT INTO public.users (id, name, email, public_key) VALUES (2, 'b', 'b@b.com', 'bkey');
-INSERT INTO public.users (id, name, email, public_key) VALUES (3, 'c', 'c@c.com', 'ckey');
-INSERT INTO public.users (id, name, email, public_key) VALUES (4, 'd', 'd@d.com', 'dkey');
-INSERT INTO public.users (id, name, email, public_key) VALUES (5, 'e', 'e@e.com', 'ekey');
-INSERT INTO public.users (id, name, email, public_key) VALUES (6, 'John', 'John@john.com', 'johnpk');
+INSERT INTO public.users (name, email, public_key) VALUES ('a', 'a@a.com', 'akey');
+INSERT INTO public.users (name, email, public_key) VALUES ('b', 'b@b.com', 'bkey');
+INSERT INTO public.users (name, email, public_key) VALUES ('c', 'c@c.com', 'ckey');
+INSERT INTO public.users (name, email, public_key) VALUES ('d', 'd@d.com', 'dkey');
+INSERT INTO public.users (name, email, public_key) VALUES ('e', 'e@e.com', 'ekey');
+INSERT INTO public.users (name, email, public_key) VALUES ('John', 'John@john.com', 'johnpk');
+INSERT INTO public.users (name, email, public_key) VALUES ('eg', 'Jasda', 'jeggg');
