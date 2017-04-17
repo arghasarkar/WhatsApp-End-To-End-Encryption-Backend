a3d9acbd236fcd570d3bfaaca442689e1ef231da
Argha Sarkar <argha.sarkar1994@gmail.com>
2017-04-17 11:36:35 +0100

    feat(server): Store user's phone numbers
    - Able to store user's phone numbers.
    
    - Able to search via user's phone numbers and user's name.
    
    [QA]
    Has tests: No
    Reviewed by: Nil

--- schema.sql
+++ schema.sql
@@ -3,14 +3,12 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (
 	id SERIAL PRIMARY KEY,
 	name TEXT NOT NULL,
-	email TEXT NOT NULL,
+	phone_number TEXT,
+	email TEXT,
 	public_key TEXT NOT NULL
 );
 
-INSERT INTO public.users (name, email, public_key) VALUES ('a', 'a@a.com', 'akey');
-INSERT INTO public.users (name, email, public_key) VALUES ('b', 'b@b.com', 'bkey');
-INSERT INTO public.users (name, email, public_key) VALUES ('c', 'c@c.com', 'ckey');
-INSERT INTO public.users (name, email, public_key) VALUES ('d', 'd@d.com', 'dkey');
-INSERT INTO public.users (name, email, public_key) VALUES ('e', 'e@e.com', 'ekey');
-INSERT INTO public.users (name, email, public_key) VALUES ('John', 'John@john.com', 'johnpk');
-INSERT INTO public.users (name, email, public_key) VALUES ('eg', 'Jasda', 'jeggg');
+INSERT INTO public.users (name, phone_number, email, public_key) VALUES ('a', '07554849303', 'a@a.com', 'akey');
+INSERT INTO public.users (name, phone_number, email, public_key) VALUES ('b', '07556490371', 'b@b.com', 'bkey');
+INSERT INTO public.users (name, phone_number, email, public_key) VALUES ('c', '07884657294', 'c@c.com', 'ckey');
+INSERT INTO public.users (name, phone_number, email, public_key) VALUES ('d', '07556493846', 'd@d.com', 'dkey');
